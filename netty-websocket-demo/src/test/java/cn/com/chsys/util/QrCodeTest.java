ade71e794b11fc092aef9a96e322a294d992c681
IceFoxs <2467416676@qq.com>
2018-06-27 15:43:53 +0800

    Update QrCodeTest.java

--- netty-websocket-demo/src/test/java/cn/com/chsys/util/QrCodeTest.java
+++ netty-websocket-demo/src/test/java/cn/com/chsys/util/QrCodeTest.java
@@ -9,7 +9,7 @@ import cn.hutool.extra.qrcode.QrCodeUtil;
 
 public class QrCodeTest {
 	public static void main(String[] args) throws FileNotFoundException {
-		String content ="http://10.1.2.111/netcourt/#zh-CN/phoneSign?idNumber=140429199502234410&signTokenId=0212c6c0-0bb5-4598-9f56-eba1aa195b39&timestamp=1529047344525&trialPlanId=45E1D949_9663_ABE3_581C_B6655D593F07&lastSigId=45E1D949_9663_ABE3_581C_B6655D593F07";
+		String content ="http://10.1.2.111/netcourt/#zh-CN/phoneSign?signTokenId=0212c6c0-0bb5-4598-9f56-eba1aa195b39&timestamp=1529047344525&trialPlanId=45E1D949_9663_ABE3_581C_B6655D593F07&lastSigId=45E1D949_9663_ABE3_581C_B6655D593F07";
 		generateQrCodeImage(content, 500, 500, "png", new FileOutputStream("D:/1.png"),true);
 		String resultContent =  QrCodeUtil.decode(new File("D:/1.png"));
 		System.out.println(resultContent);
